b522bad9732d9f38107e98ed92068d1b30ba2cb4
Paulo Henrique <ph.1928@hotmail.com>
2024-08-07 20:52:09 -0300

    Commit inicial

---
+++ src/main/java/org/example/exercicioapilivros/ExercicioApiLivrosApplication.java
@@ -0,0 +1,13 @@
+package org.example.exercicioapilivros;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ExercicioApiLivrosApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(ExercicioApiLivrosApplication.class, args);
+    }
+
+}
---
+++ src/test/java/org/example/exercicioapilivros/ExercicioApiLivrosApplicationTests.java
@@ -0,0 +1,13 @@
+package org.example.exercicioapilivros;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ExercicioApiLivrosApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
