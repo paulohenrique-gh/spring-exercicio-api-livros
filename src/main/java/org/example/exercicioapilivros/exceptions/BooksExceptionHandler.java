ec8a52419b0b57a651e6e7a38318e2e6a36230ce
Paulo Henrique <ph.1928@hotmail.com>
2024-08-07 21:21:45 -0300

    Adiciona exceção a ser tratada

--- src/main/java/org/example/exercicioapilivros/exceptions/BooksExceptionHandler.java
+++ src/main/java/org/example/exercicioapilivros/exceptions/BooksExceptionHandler.java
@@ -2,6 +2,8 @@ package org.example.exercicioapilivros.exceptions;
 
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
+import org.springframework.validation.FieldError;
+import org.springframework.web.bind.MethodArgumentNotValidException;
 import org.springframework.web.bind.MissingServletRequestParameterException;
 import org.springframework.web.bind.annotation.ExceptionHandler;
 import org.springframework.web.bind.annotation.RestControllerAdvice;
@@ -24,5 +26,20 @@ public class BooksExceptionHandler {
         Map<String, String> details = new HashMap<>();
         details.put("error", ex.getMessage());
         return ResponseEntity.status(HttpStatus.BAD_REQUEST).body(details);
+    }
+
+    @ExceptionHandler(MethodArgumentNotValidException.class)
+    public ResponseEntity<Map<String, Map<String, String>>> handleMethodArgumentNotValidException(MethodArgumentNotValidException ex) {
+        Map<String, String> detailedErrors = new HashMap<>();
+        Map<String, Map<String, String>> errors = new HashMap<>();
+        errors.put("errors", detailedErrors);
+
+        ex.getBindingResult().getAllErrors().forEach((error) -> {
+            String fieldName = ((FieldError) error).getField();
+            String errorMessage = error.getDefaultMessage();
+            detailedErrors.put(fieldName, errorMessage);
+        });
+
+        return ResponseEntity.status(HttpStatus.BAD_REQUEST).body(errors);
     }
 }
--- src/main/java/org/example/exercicioapilivros/repositories/BooksRepository.java
+++ src/main/java/org/example/exercicioapilivros/repositories/BooksRepository.java
@@ -2,8 +2,14 @@ package org.example.exercicioapilivros.repositories;
 
 import org.example.exercicioapilivros.models.Book;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
+import org.springframework.data.repository.query.Param;
 
+import java.util.List;
 import java.util.UUID;
 
 public interface BooksRepository extends JpaRepository<Book, UUID> {
+
+    @Query("SELECT b FROM Book b WHERE b.genre ILIKE %:genre%")
+    List<Book> findByGenre(@Param("genre")String genre);
 }
